b4c24a8ef62c0f84fe2675e78f8f4472a1c0023e
mdimitrova <monicabdimitrova@gmail.com>
2015-03-26 22:03:06 +0200

    Implement creating of a post, add a Post class

--- week2/microblog/microblog.rb
+++ week2/microblog/microblog.rb
@@ -1,17 +1,28 @@
 require 'sinatra/base'
 require 'bundler/setup'
+require './Post.rb'
 
 class Microblog < Sinatra::Application
-  before do
-    posts = []
-  end
+  @@posts = []
 
   get '/' do
-    haml :home, locals: {:welcome_message => 'Welcome to microblog :)'}
+    haml :home, locals: {welcome_message: 'Welcome to microblog :)', posts: @@posts}
   end
 
   not_found do
     haml :not_found
+  end
+
+  get '/create' do
+    haml :create
+  end
+
+  post '/create' do
+    new_post = Post.new()
+    new_post.title = params[:title]
+    new_post.text = params[:text]
+    @@posts << new_post
+    redirect '/'
   end
 
   run! if app_file == $0

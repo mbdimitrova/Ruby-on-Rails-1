b7a9dd5b73d0f5fc9bf01bab480746f6f54fc2fa
molly <monicabdimitrova@gmail.com>
2015-04-17 12:13:53 +0300

    Rename root view

--- week3/1-microblog/config/routes.rb
+++ week3/1-microblog/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root 'posts#index'
-  resources :posts
+  get 'welcome/index'
+
+  root 'welcome#index'
+  resources :welcome
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
